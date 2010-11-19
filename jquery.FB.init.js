efb9f0b9a84695fd872a67e83947331a3bb5dac6
Simon Schmid <ssc@feinheit.ch>
2010-11-19 01:23:45 +0100

    get german fbjs

--- jquery.FB.init.js
+++ jquery.FB.init.js
@@ -1,5 +1,5 @@
 $(function(){
-    $.getScript(document.location.protocol + '//connect.facebook.net/en_US/all.js', function() {
+    $.getScript(document.location.protocol + '//connect.facebook.net/de_DE/all.js', function() {
         FB.init({appId: $('#FACEBOOK_APP_ID').val(), status: true, cookie: true, xfbml: true});
         FB.ready = true;
         $(document).trigger('FB.ready');
